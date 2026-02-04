caeb1fd9e8a64a34b93ff3f597305f6257926795
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-04 13:28:34 +0100

    Create both computer and human choice functions for the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3) + 1;
+    switch (computerChoice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+};
+
+console.log(`Computer choose: ${getComputerChoice()}`);
+
+function getHumanChoice() {
+    humanChoice = prompt("Choose between rock, paper or scissors: ")
+    return humanChoice;
+}
+
+console.log(`Human choose: ${getHumanChoice()}`);
